cec897906af9c1ef0f43b33b527581b4bdf8c8f0
Archit Jain <architjain2005@gmail.com>
2024-02-09 08:47:15 +0530

    initial

---
+++ filecopy/filecopy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FileCopy
+{
+    class filecopy
+    {
+        static void Main(String[] args)
+        {
+            FileStream f1 = new FileStream("F:\\T001_A\\MIS\\Analytical Software linked with video for precise game judgement.docx", FileMode.OpenOrCreate);
+            FileStream f2 = new FileStream("F:\\T001_A\\MIS\\copied.docx", FileMode.OpenOrCreate);
+            int i;
+            while ((i = f1.ReadByte()) != -1)
+            {
+                f2.WriteByte((byte)i);
+            }
+            Console.WriteLine("File copied");
+            f1.Close();
+            f2.Close();
+            Console.ReadKey();
+        }
+    }
+}
+
